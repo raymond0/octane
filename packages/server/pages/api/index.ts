ba2b02a295ce33c874c5ea6f29bd7c0e58ac8e00
Ray Hunter <r@ymon.do>
2025-04-15 16:20:10 +0100

    Don't return the RPC url in the /api call.

--- packages/server/pages/api/index.ts
+++ packages/server/pages/api/index.ts
@@ -2,9 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import config from '../../../../config.json';
 import { cors, ENV_FEE_PAYER, rateLimit } from '../../src';
 
+const { rpcUrl, ...configWithoutRpcUrl } = config;
 const body = {
     feePayer: ENV_FEE_PAYER.toBase58(),
-    ...config,
+    ...configWithoutRpcUrl,
 };
 
 // Endpoint to get Octane's configuration
